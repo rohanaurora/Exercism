79a65708b99d1803c8f1396794de8f35e266f1be
Rohan Aurora <rohanaurora@gmail.com>
2020-10-16 22:17:42 -0700

    Gigasecond

--- gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -1,5 +1,11 @@
 //Solution goes in Sources
 
+//Calculate the moment when someone has lived for 10^9 seconds.
+//
+//A gigasecond is 10^9 (1,000,000,000) seconds.
+//
+
+
 import Foundation
 
 struct Gigasecond {
